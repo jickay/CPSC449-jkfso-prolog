9e63e0799a5ef301db9be87d2f5e651541158276
jickay <jickay@gmail.com>
2018-04-01 16:19:44 -0600

    Too near working.

--- hardconstraints.pl
+++ hardconstraints.pl
@@ -9,10 +9,37 @@
 % Check for forced/forbidden conflicts, makes invalid solution
 
 % Check for too-near invalid pairs
-tooNear(_,[]).
-tooNear(Matches,TooNear):-
-    hasTooNear(M,TooNear)
+hasTooNear(_,[]):- false.
+hasTooNear(Matches,TooNear,I):-
+    nth1(I,Matches,Task),
+    checkLeft(Task,Matches,TooNear) -> true ;
+    checkRight(Task,Matches,TooNear) -> true ;
+    J is I+1,
+    hasTooNear(Matches,TooNear,J).
 
-hasTooNear(M,[Left,Right|TooNear]):-
-    M = Left,
+checkLeft(_,_,[]):- false.
+checkLeft(M,Matches,[[Left,_]|TooNear]):-
+    indexOf(Matches,M,MIndex),
+    getLeftIndex(MIndex,Index),
+    nth1(Index,Matches,MLeft),
+    checkSame(MLeft,Left) -> true ; checkLeft(M,Matches,TooNear).
 
+checkRight(_,_,[]):- false.
+checkRight(M,Matches,[[_,Right]|TooNear]):-
+    indexOf(Matches,M,MIndex),
+    getRightIndex(MIndex,Index),
+    nth1(Index,Matches,MRight),
+    checkSame(MRight,Right) -> true ; checkRight(M,Matches,TooNear).
+
+indexOf([Element|_], Element, 1):- !.
+indexOf([_|Tail], Element, Index):-
+    indexOf(Tail, Element, Index1), !,
+    Index is Index1+1.
+
+getLeftIndex(Index,IndexL):-
+    Index > 1 -> IndexL is Index-1 ; IndexL is 8.
+
+getRightIndex(Index,IndexR):-
+        Index < 8 -> IndexR is Index+1 ; IndexR is 1.
+
+checkSame(X,Y):- X = Y.
