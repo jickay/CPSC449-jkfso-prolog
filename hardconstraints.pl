6299a682f7be2d1624c94fbc636d7f0fb536b1f4
Jacky Tang <jickay@gmail.com>
2018-04-03 19:27:12 -0600

    Too-near for list of matches. In progress.

--- hardconstraints.pl
+++ hardconstraints.pl
@@ -9,27 +9,41 @@
 % Check for forced/forbidden conflicts, makes invalid solution
 
 % Check for too-near invalid pairs
-hasTooNear(_,[]):- false.
-hasTooNear(Matches,TooNear,I):-
-    nth1(I,Matches,Task),
-    checkLeft(Task,Matches,TooNear) -> true ;
-    checkRight(Task,Matches,TooNear) -> true ;
-    J is I+1,
-    hasTooNear(Matches,TooNear,J).
+tooNear([],_,_).
+tooNear([Matches|ListOfMatches],TooNear,Matches):-
+    % head(ListOfMatches,Matches),
+    noTooNear(Matches,Matches,TooNear) ->
+    tooNear(ListOfMatches,TooNear,[]) ;
+    tooNear(ListOfMatches,TooNear,Matches), true.
 
-checkLeft(_,_,[]):- false.
-checkLeft(M,Matches,[[Left,_]|TooNear]):-
+% See if too-near violation in a set of matches
+noTooNear(_,[],_).
+noTooNear(Matches,[M|MTail],TooNear):-
+    % head(Matches,M),
+    % tail(Matches,MTail),
+    checkLeft(M,Matches,TooNear),
+    checkRight(M,Matches,TooNear),
+    noTooNear(Matches,MTail,TooNear).
+
+checkLeft(_,_,[]).
+checkLeft(M,Matches,[[Left,Right]|TooNear]):-
+    checkSame(M,Right) ->
     indexOf(Matches,M,MIndex),
     getLeftIndex(MIndex,Index),
     nth1(Index,Matches,MLeft),
-    checkSame(MLeft,Left) -> true ; checkLeft(M,Matches,TooNear).
+    checkDiff(MLeft,Left),
+    checkLeft(M,Matches,TooNear) ;
+    checkLeft(M,Matches,TooNear).
 
-checkRight(_,_,[]):- false.
-checkRight(M,Matches,[[_,Right]|TooNear]):-
+checkRight(_,_,[]).
+checkRight(M,Matches,[[Left,Right]|TooNear]):-
+    checkSame(M,Left) ->
     indexOf(Matches,M,MIndex),
     getRightIndex(MIndex,Index),
     nth1(Index,Matches,MRight),
-    checkSame(MRight,Right) -> true ; checkRight(M,Matches,TooNear).
+    checkDiff(MRight,Right),
+    checkRight(M,Matches,TooNear) ;
+    checkRight(M,Matches,TooNear).
 
 indexOf([Element|_], Element, 1):- !.
 indexOf([_|Tail], Element, Index):-
@@ -42,4 +56,8 @@ getLeftIndex(Index,IndexL):-
 getRightIndex(Index,IndexR):-
         Index < 8 -> IndexR is Index+1 ; IndexR is 1.
 
-checkSame(X,Y):- X = Y.
+checkSame(X,Y):- X == Y.
+checkDiff(X,Y):- X \== Y.
+
+head([X|_],X).
+tail([_|X],X).
