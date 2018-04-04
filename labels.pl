c0c81695c486a00cbd6d81e934741c13600be743
sceveleigh <sceveleigh@gmail.com>
2018-04-04 17:59:32 -0600

    Added label checking file

---
+++ labels.pl
@@ -0,0 +1,27 @@
+%define module
+:- module(labels,[checkLabels/1]).
+
+%Import readLines
+:- use_module(input_output,[readLines/2,printErrorAndClose/2]).
+
+label('Name:').
+label('forced partial assignment:').
+label('forbidden machine:').
+label('too-near tasks:').
+label('machine penalties:').
+label('too-near penalities:').
+
+%Takes in list of lines from input, checks if the labels are all there
+checkLabels(ListOfLines):-
+        label(X), is_member(X, ListOfLines).
+
+%Recursively checks if X is a member of a list
+is_member(_,[]):-
+    open('ErrorWhileParsingInputFile.txt',read,ErrorStream),
+    readLines(ErrorStream,ErrorTerm),
+    open('output.txt',write,OutputStream),
+    printErrorAndClose(OutputStream,ErrorTerm).
+is_member(X,[X|_]):- !.
+is_member(X,[_|Ys]):-
+    is_member(X,Ys).
+
