d1f18a22ecf6e148adab91a5b670334dfd2b8785
sceveleigh <sceveleigh@gmail.com>
2018-04-09 00:54:09 -0600

    Added some possibly useful predicates

--- labels.pl
+++ labels.pl
@@ -23,3 +23,28 @@ is_member(X,[X|_]):- !.
 is_member(X,[_|Ys]):-
     is_member(X,Ys).
 
+split(List, Pivot, Left, Right) :- append(Left, [Pivot|Right], List).
+
+% Partial2 = forced pairs
+get_forced_partial(List, Partial2):-
+    split(List, "forced partial assignment:", _, Partial1),
+    split(Partial1, "forbidden machine:", Partial2, _).
+
+% Partial2 = forbidden machines      
+get_forbidden_machine(List, Partial2):-
+    split(List, "forbidden machine:", _, Partial1),
+    split(Partial1, "too-near tasks:", Partial2, _).
+    
+% Partial2 = too near tasks
+get_toonear_tasks(List, Partial2):-
+    split(List, "too-near tasks:", _, Partial1),
+    split(Partial1, "machine penalties:", Partial2, _).
+
+% Partial2 = machine penalties
+get_machine_penalties(List, Partial2):-
+    split(List, "machine penalties:", _, Partial1),
+    split(Partial1, "too-near penalities", Partial2, _).
+
+% Partial2 = too near penalties
+get_toonear_penalties(List, Partial2):-
+    split(List, "too-near penalities", _, Partial2).
